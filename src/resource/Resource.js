f7243cb31d53e462b103ace0ed72496a3daa42e6
Kevin Linera <kevin.linera@upr.edu>
2023-04-22 19:31:04 -0500

    fixed bleeding upvote instances

--- src/resource/Resource.js
+++ src/resource/Resource.js
@@ -10,7 +10,8 @@ import "swiper/css/pagination";
 import {Pagination} from "swiper"
 import { Swiper, SwiperSlide } from "swiper/react";
 import {AuthContext} from "../auth/AuthContext";
-import Upvote from "../upvote/Upvote";
+import Blog from "../blog/Blog"
+
 function Resource() {
 
     const {currentUser} = useContext((AuthContext))
@@ -147,28 +148,6 @@ function Resource() {
 }
 export default Resource;
 
-function Blog(props) {
-
-    const handleButtonClick = () => {
-        window.open("https://" + props.data.link, '_blank');
-      };
-
-    return <div className="Blog flex overflow-hidden justify-center">
-        
-            <img className=" w-1/2" src="mchine.jpg" alt="logo"></img>
-            <div className="Blog_container w-1/2 overflow-hidden">
-                <h1 className="Blog_tittle">{props.data.title}
-                </h1>
-                <h2 className="Blog_information">{props.data.information} {console.log(props.data)}</h2>
-                <div className="Blog_button_container space-x-3">
-                    <Upvote data={{blog_id: props.data.blog_id, upvote: props.data.upvote}}/>
-                    <button className="Blog_button rounded-lg font-bold" onClick={handleButtonClick}>
-                        Read
-                    </button>
-                </div>
-            </div>
-        </div>
-}
 
 function Organization(){
     return <div className="Organization flex overflow-hidden justify-center">
