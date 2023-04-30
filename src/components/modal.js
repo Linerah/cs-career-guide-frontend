15fcf2a2dc9539c6d56a5b6db0b49fd4cbe18820
Andre Clavell <prandre97@yahoo.com>
2023-04-30 17:18:29 -0400

    more carousel work done

--- src/components/modal.js
+++ src/components/modal.js
@@ -4,7 +4,7 @@ function Modal({ isOpen, onClose, children }) {
   const modalStyle = isOpen ? "block" : "hidden";
 
   return (
-    <div className={modalStyle + " fixed inset-0 bg-gray-500 bg-opacity-75"}>
+    <div className={modalStyle + " fixed inset-0 bg-gray-500 bg-opacity-75 z-50"}>
       <div className="fixed inset-0">
         <div className="flex items-center justify-center h-screen">
           <div className="bg-colegio-green rounded-lg shadow-lg p-6 w-1/3 h-3/8 relative">
