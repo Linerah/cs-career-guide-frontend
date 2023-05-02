05037b82fb39a88a9208361405ea89a9e0c2207f
fernandoramos9 <fernando.ramos9@upr.edu>
2023-05-02 19:53:16 -0400

    Removed flowbite (it broke things)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
    content: [
      "./src/**/*.{js,jsx,ts,tsx}",
-       "./node_modules/flowbite/**/*.js"
    ],
    theme: {
      extend: {
@@ -17,6 +16,5 @@ module.exports = {
      },
    },
    plugins: [
-       require('flowbite/plugin')
    ],
  }
